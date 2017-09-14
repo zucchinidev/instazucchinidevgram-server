5648dc690d9a64308672d4c98a3aaa3344a2a9bc
Andrea Zucchini <azga84@gmail.com>
2017-09-14 23:23:08 +0200

    hash tags extractor service

--- src/HashTags/Application/HashTagsExtractorService.ts
+++ src/HashTags/Application/HashTagsExtractorService.ts
@@ -3,6 +3,22 @@ import { HashTagsExtractorCommand } from './HashTagsExtractorCommand'
 export class HashTagsExtractorService {
 
   perform (hashTagsExtractorCommand: HashTagsExtractorCommand): Array<string> {
-    return [hashTagsExtractorCommand.getMessage()]
+    const message = hashTagsExtractorCommand.getMessage()
+
+    if (message == null) {
+      return []
+    }
+
+    const hashTagRegExp = /#(\w+)/g
+    const matches = message.match(hashTagRegExp)
+    if (matches === null) {
+      return []
+    }
+    return matches.map(this.normalizeHashTags)
+  }
+
+  private normalizeHashTags (match: string): string {
+    match = match.toLowerCase()
+    return match.replace(/#/g, '')
   }
 }
