25d5d73afcdb9556d6c9d08e0c3bd4ea13a26e40
Andrea Zucchini <azga84@gmail.com>
2017-09-15 14:57:06 +0200

    init test for save image

---
+++ test/SaveImageService-test.ts
@@ -0,0 +1,8 @@
+import { test } from 'ava'
+import { v4 } from 'uuid'
+
+const uuid = require('uuid-base62')
+const dbName = `instazucchinidevgram_${v4()}`
+test('extracting hashtags from test', t => {
+
+})
