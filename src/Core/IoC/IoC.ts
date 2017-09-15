efc3804c1116d51103b33f1e96c624beff8848b3
Andrea Zucchini <azga84@gmail.com>
2017-09-15 14:55:54 +0200

    init inversion of control

---
+++ src/Core/IoC/IoC.ts
@@ -0,0 +1,6 @@
+// import { Container } from 'inversify'
+// import { MongoRepository } from '../../Infrastructure/Persistence/Mongo/MongoRepository'
+// import { TYPES } from './Types'
+//
+// const container = new Container()
+// container.bind<MongoRepository<>>(TYPES.MongoRepository).to(MongoRepository)
---
+++ src/Core/IoC/Types.ts
@@ -0,0 +1,3 @@
+export const TYPES = {
+  MongoRepository: Symbol('MongoRepository')
+}
